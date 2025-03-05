7689095f856907355e1eafb2d9346d9f6d8c0edd
lindacelmaa <lindacelma98@gmail.com>
2025-03-05 12:09:50 +0200

    Second page

--- src/main/java/lv/venta/controller/MyFirstController.java
+++ src/main/java/lv/venta/controller/MyFirstController.java
@@ -1,6 +1,9 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,9 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class MyFirstController {
 	
+	private Random rand = new Random();
 	@GetMapping("/simple") //localhost:8080/simple
 	public String myFirstGetController() {
 		System.out.println("Pirmais kontrolieris nostradaja");
 		return "simple-page"; //paraadiis simple-page.html lapu
 	}
+	
+	@GetMapping("/getdata")
+	public String getControllerSendData(Model model) {
+		System.out.println("Send data kontrolieris nostr'ad'aja");
+		String data = "Linda ->" + rand.nextInt(0, 101);
+		model.addAttribute("package", data);
+		return "show-data-page";
+	}
 }
+
