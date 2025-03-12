e81739a603307e231a76b579ad4b805df70b17ee
lindacelmaa <lindacelma98@gmail.com>
2025-03-12 11:11:41 +0200

    Entities etc

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,4 +1,9 @@
 package lv.venta.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import lombok.AccessLevel;
@@ -11,12 +16,25 @@ import lombok.ToString;
 @Setter
 @NoArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 public class Product {
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "Id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@Column(name = "title")
 	private String title;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@Column(name = "price")
 	private float price;
+	
+	@Column(name = "quantity")
 	private int quantity;
 	
 	public Product(String title, String description, float price, int quantity) {
