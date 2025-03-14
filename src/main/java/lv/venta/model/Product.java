96256057077c6a0b63ab9a7416507f4d043504f7
lindacelmaa <lindacelma98@gmail.com>
2025-03-14 09:22:40 +0200

    New dependency

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -5,6 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import lombok.AccessLevel;
@@ -27,15 +32,24 @@ public class Product {
 	private long id;
 	
 	@Column(name = "title")
+	@NotNull
+	@Pattern(regexp = "[A-Z]{1}[a-z ]{2,15}")
+	//@Size(min=3, max = 16)
 	private String title;
 	
 	@Column(name = "description")
+	@NotNull
+	@Pattern(regexp = "[A-Za-z :;]{3,30}")
 	private String description;
 	
 	@Column(name = "price")
+	@Min(0)
+	@Max(1000)
 	private float price;
 	
 	@Column(name = "quantity")
+	@Min(0)
+	@Max(1000)
 	private int quantity;
 	
 	public Product(String title, String description, float price, int quantity) {
