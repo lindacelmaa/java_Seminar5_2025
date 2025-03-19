a35d529888d75be064f7fdafe2055dc31ce1190e
lindacelmaa <lindacelma98@gmail.com>
2025-03-19 09:16:15 +0200

    Fixed code

--- src/main/java/lv/venta/controller/ProductCRUDController.java
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -69,7 +70,7 @@ public class ProductCRUDController {
 		return "create-product"; 
 		
 	}
-	
+	@PostMapping("/create")
 	public String postControllerCreateNewProduct(Product product, Model model) {
 		try {
 			System.out.println(product);
