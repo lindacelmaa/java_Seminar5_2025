4414009456e0017b5a13ead7fc875275f66ca72d
Linda Celma <lindacelma98@gmail.com>
2025-03-21 00:34:46 +0200

    Fixes

--- src/main/java/lv/venta/controller/ProductFiltAndCalcController.java
+++ src/main/java/lv/venta/controller/ProductFiltAndCalcController.java
@@ -1,6 +1,75 @@
 package lv.venta.controller;
-
-public class ProductFiltAndCalcController {
-	@Controller
-	@RequestMapping("/product/process");
-}
+ 
+ import java.util.ArrayList;
+ 
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Controller;
+ import org.springframework.ui.Model;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PathVariable;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ 
+ import lv.venta.model.Product;
+ import lv.venta.service.IProductFilterService;
+ 
+ @Controller
+ @RequestMapping("/product/process")
+ public class ProductFiltAndCalcController {
+ 	
+ 	@Autowired
+ 	private IProductFilterService prodService;
+ 	
+ 	@GetMapping("/price/{threshold}")//localhost:8080/product/process/price/2.77
+ 	public String getControllerProductPriceLessThan
+ 	(@PathVariable(name ="threshold") float threshold, Model model)
+ 	{
+ 		try {
+ 			ArrayList<Product> filteredProducts = prodService.getAllProductsWherePriceLessThan(threshold);
+ 			model.addAttribute("package", filteredProducts);
+ 			return "show-multiple-products";
+ 		} catch (Exception e) {
+ 			model.addAttribute("package", e.getMessage());
+ 			return "show-error";
+ 		}
+ 		
+ 		
+ 	}
+ 	
+ 	@GetMapping("/quantity/{threshold}") // localhost:8080/product/process/quantity/10
+    public String getControllerProductQuantityMoreThan(@PathVariable(name = "threshold") int threshold, Model model) {
+        try {
+            ArrayList<Product> filteredProducts = prodService.getAllProductsWhereQuantityMoreThan(threshold);
+            model.addAttribute("package", filteredProducts);
+            return "show-multiple-products";
+        } catch (Exception e) {
+            model.addAttribute("package", e.getMessage());
+            return "show-error";
+        }
+    }
+ 	
+ 	 @GetMapping("/search/{text}") // localhost:8080/product/process/search/laptop
+     public String getControllerProductTitleOrDescriptionContains(@PathVariable(name = "text") String text, Model model) {
+         try {
+             ArrayList<Product> filteredProducts = prodService.getAllProductsWhereTitleOrDescriptionContains(text);
+             model.addAttribute("package", filteredProducts);
+             return "show-multiple-products";
+         } catch (Exception e) {
+             model.addAttribute("package", e.getMessage());
+             return "show-error";
+         }
+     }
+ 	 
+ 	@GetMapping("/income") // localhost:8080/product/process/income
+    public String getControllerIncomeFromProducts(Model model) {
+        try {
+            float income = prodService.getIncomeFromProducts();
+            model.addAttribute("package", income);
+            return "show-income";
+        } catch (Exception e) {
+            model.addAttribute("package", e.getMessage());
+            return "show-error";
+        }
+    }
+ 	
+ 
+ }
